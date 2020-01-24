951383ccc36efc95a19567f0d22d6c5381945c4a
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-24 04:50:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc
+array = ["red lobster", "mcdonalds","shake shack", "burger king"]
+array.sort do |a, b|
+a <=> b
+end
 
+
+def sort_array_desc
+  array = ["red lobster", "mcdonalds", "shake shack", "burger king"]
+  array.sort.reverse do |a, b|
+    a <=> b 
+  end
+end
+
+def sort_array_char_count
+array = ["Garfield", "Maru", "Puss", "Cat in the Hat"]
+array.sort.length do |a, b|
+  a <=> b 
+end
+
+def swap_elements
+array = ["Bowie", "Jackson", "Prince", "Franklin"]
+array.sort.swap.even
+a <=> b 
+end
+
+def reverse_array
+  
