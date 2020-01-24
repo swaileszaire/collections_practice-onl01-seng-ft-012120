fc4f1068e41bdf0668b0c54076f7ad0fe32ed1e3
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-24 05:09:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc
+array = ["red lobster", "mcdonalds","shake shack", "burger king"]
+array.sort do |a, b|
+a <=> b
+end
 
+
+def sort_array_desc
+  array = ["red lobster", "mcdonalds", "shake shack", "burger king"]
+  array.sort.reverse do |a, b|
+    a <=> b 
+  end
+end
+
+def sort_array_char_count
+array = ["Garfield", "Maru", "Puss", "Cat in the Hat"]
+array.sort.length do |a, b|
+  a <=> b 
+end
+
+def swap_elements
+array = ["Bowie", "Jackson", "Prince", "Franklin"]
+array.sort.swap.even
+a <=> b 
+end
+
+def reverse_array
+array = ["phone", "tablet", "computer", "television"]
+array.sort.reverse do |a, b|
+  a <=> b
+end
+
+def kesha_maker
+array = ["Andre", "Jade", "Robbie", "Cat"].each |w|
+counter = 3 
+counter +=
+puts do |w| "$"
+end
+
+def find_a
+array = ["Andrea", "Anthony", "Ethan", "Agnis"]
+array.find do |word|
+  find.delete_if == < "a"
+end
+
+def sum_array
+array [1, 2, 3, 4, 5, 6, 7]
+array.add do |num|
+end
+
+def add_s
+array = ["Guy", "Girl", "Mother", "Father"]
+array.add "s" do |num|
+end
